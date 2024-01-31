6d5e63f4b5b477028c4ab206d24cc49d6bec9348
Andrew Gray <andrewg@mirageoscience.com>
2024-01-31 12:34:30 -0800

    set versions to 0.1.0-alpha.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "octree-creation"
-release = "0.1.0-alpha.1"
+release = "0.1.0-alpha.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
