9cc7bbc9ac65e440c74085fb964075a118442be8
benjamink <benjamink@mirageoscience.com>
2024-05-31 13:49:39 -0700

    augment .constants/template_dict with new types

--- octree_creation_app/constants.py
+++ octree_creation_app/constants.py
@@ -14,13 +14,18 @@ template_dict: dict[str, dict] = {
         "group": "Refinement A",
         "label": "Object",
         "meshType": [
-            "{202C5DB1-A56D-4004-9CAD-BAAFD8899406}",
-            "{6A057FDC-B355-11E3-95BE-FD84A7FFCB88}",
-            "{F26FEBA3-ADED-494B-B9E9-B2BBCBE298E1}",
+            "{202c5db1-a56d-4004-9cad-baafd8899406}",
+            "{6a057fdc-b355-11e3-95be-fd84a7ffcb88}",
+            "{f26feba3-aded-494b-b9e9-b2bbcbe298e1}",
             "{b99bd6e5-4fe1-45a5-bd2f-75fc31f91b38}",
             "{0b639533-f35b-44d8-92a8-f70ecff3fd26}",
             "{9b08bb5a-300c-48fe-9007-d206f971ea92}",
             "{19730589-fd28-4649-9de0-ad47249d9aba}",
+            "{b3a47539-0301-4b27-922e-1dde9d882c60}",
+            "{a81c6b0a-f290-4bc8-b72d-60e59964bfe8}",
+            "{41018a45-01a0-4c61-a7cb-9f32d8159df4}",
+            "{deebe11a-b57b-4a03-99d6-8f27b25eb2a8}",
+            "{275ecee9-9c24-4378-bf94-65f3c5fbe163}"
         ],
         "value": None,
         "tooltip": "Object used to refine the mesh. Refinement strategy varies "
